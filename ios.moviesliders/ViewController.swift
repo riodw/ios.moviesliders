9a5c6024d653fadcb84276beb8cd24dd029f0874
Rio Weber <rio@medtricslab.com>
2018-01-08 21:00:25 -0500

    Update UI

--- ios.moviesliders/ViewController.swift
+++ ios.moviesliders/ViewController.swift
@@ -17,14 +17,22 @@ import LBTAComponents
 class ViewController: DatasourceController {
     
     let step: Float = 2 // UISlider to snap to steps
+    let sliderMinimumValue: Float = 0
+    let sliderMaximumValue: Float = 100
+    
 //    var roundedStepValue: Float = 0
     var timer = Timer()
-    let mySlider = UISlider(frame:CGRect(x: 0, y: 0, width: 340, height: 30))
-    let mySlider2 = UISlider(frame:CGRect(x: 10, y: 200, width: 300, height: 20))
-    let btn = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
-    let btn2 = UIButton(frame: CGRect(x: 100, y: 500, width: 100, height: 50))
+    // Sliders
+    let interest = UISlider()
+    let cliche = UISlider()
+    let funny = UISlider()
+    let dumb = UISlider()
+    let wtf = UISlider()
+    // Buttons
+    let btnStart = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
+    let btnPlay = UIButton(frame: CGRect(x: 100, y: 500, width: 100, height: 50))
     
-    let sampleTextField =  UITextField(frame: CGRect(x: 20, y: 100, width: 150, height: 40))
+    let movieTextField =  UITextField(frame: CGRect(x: 0, y: 0, width: 150, height: 40))
     
     let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
     
@@ -45,92 +53,106 @@ class ViewController: DatasourceController {
         super.viewDidLoad()
         /* UI */
         collectionView?.backgroundColor = .white
-        
-
-        
+    
         
         /* INIT OBJECTS */
-        
         self.w = view.bounds.width
         self.y = view.bounds.height
         
         self.view.frame =  CGRect(x:0, y: 0, width: 100, height: y)
         
-        
-        mySlider.minimumValue = 1
-        mySlider.maximumValue = 100
-        mySlider.isContinuous = true
-        mySlider.tintColor = UIColor.red
-        mySlider.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
+        // Interest
+        interest.minimumValue = sliderMinimumValue
+        interest.maximumValue = sliderMaximumValue
+        interest.isContinuous = true
+        interest.tintColor = UIColor.red
+        interest.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
         // width (height)
-        mySlider.bounds.size.width = y-150
-        mySlider.center = CGPoint(x: w/2-25, y: y/2+10)
-        mySlider.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+        interest.bounds.size.width = y-150
+        interest.center = CGPoint(x: w/2-((20*5)-(0*50)), y: y/2+10)
+        interest.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
         
-        mySlider2.minimumValue = 1
-        mySlider2.maximumValue = 100
-        mySlider2.isContinuous = true
-        mySlider2.tintColor = UIColor.green
-        mySlider2.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
-        // width (height)
-        mySlider2.bounds.size.width = y-150
-        mySlider2.center = CGPoint(x: w/2+25, y: y/2+10)
-        mySlider2.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+        // Cliche
+        cliche.minimumValue = sliderMinimumValue
+        cliche.maximumValue = sliderMaximumValue
+        cliche.isContinuous = true
+        cliche.tintColor = UIColor.green
+        cliche.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
+        cliche.bounds.size.width = y-150
+        cliche.center = CGPoint(x: w/2-((20*5)-(1*50)), y: y/2+10)
+        cliche.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+        
+        // Funny
+        funny.minimumValue = sliderMinimumValue
+        funny.maximumValue = sliderMaximumValue
+        funny.isContinuous = true
+        funny.tintColor = UIColor.blue
+        funny.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
+        funny.bounds.size.width = y-150
+        funny.center = CGPoint(x: w/2-((20*5)-(2*50)), y: y/2+10)
+        funny.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+        
+        // Dumb
+        dumb.minimumValue = sliderMinimumValue
+        dumb.maximumValue = sliderMaximumValue
+        dumb.isContinuous = true
+        dumb.tintColor = UIColor.purple
+        dumb.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
+        dumb.bounds.size.width = y-150
+        dumb.center = CGPoint(x: w/2-((20*5)-(3*50)), y: y/2+10)
+        dumb.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+
+        // WTF
+        wtf.minimumValue = sliderMinimumValue
+        wtf.maximumValue = sliderMaximumValue
+        wtf.isContinuous = true
+        wtf.tintColor = UIColor.yellow
+        wtf.addTarget(self, action: #selector(sliderValueDidChange(_:)), for: .valueChanged)
+        wtf.bounds.size.width = y-150
+        wtf.center = CGPoint(x: w/2-((20*5)-(4*50)), y: y/2+10)
+        wtf.transform = CGAffineTransform(rotationAngle: CGFloat(3 * Double.pi / 2))
+
+        
+        // BUTTONS
+        // Start / Stop
+        btnStart.backgroundColor = UIColor.green
+        btnStart.setTitle("START", for: .normal)
+        btnStart.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        btnStart.tag = 1
+        btnStart.frame.origin = CGPoint(x: 0, y: y-10)
+        // Play / Pause
+        btnPlay.backgroundColor = UIColor.green
+        btnPlay.setTitle("Play", for: .normal)
+        btnPlay.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
+        btnPlay.tag = 1
+        btnPlay.frame.origin = CGPoint(x: w-100, y: y-10)
         
         
-        btn.backgroundColor = UIColor.green
-        btn.setTitle("START", for: .normal)
-        btn.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        btn.tag = 1
-        btn.frame.origin = CGPoint(x: 0, y: y-10)
+        movieTextField.placeholder = self.movie_name
+        movieTextField.font = UIFont.systemFont(ofSize: 15)
+        movieTextField.borderStyle = UITextBorderStyle.roundedRect
+        movieTextField.autocorrectionType = UITextAutocorrectionType.no
         
+        movieTextField.keyboardType = UIKeyboardType.default
+        movieTextField.returnKeyType = UIReturnKeyType.done
+        movieTextField.clearButtonMode = UITextFieldViewMode.whileEditing;
+        movieTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
         
-        btn2.backgroundColor = UIColor.green
-        btn2.setTitle("Play", for: .normal)
-        btn2.addTarget(self, action: #selector(buttonAction2), for: .touchUpInside)
-        btn2.tag = 1
-        btn2.frame.origin = CGPoint(x: w-100, y: y-10)
-        
-        
-        sampleTextField.placeholder = self.movie_name
-        sampleTextField.font = UIFont.systemFont(ofSize: 15)
-        sampleTextField.borderStyle = UITextBorderStyle.roundedRect
-        sampleTextField.autocorrectionType = UITextAutocorrectionType.no
-        
-        sampleTextField.keyboardType = UIKeyboardType.default
-        sampleTextField.returnKeyType = UIReturnKeyType.done
-        sampleTextField.clearButtonMode = UITextFieldViewMode.whileEditing;
-        sampleTextField.contentVerticalAlignment = UIControlContentVerticalAlignment.center
-        
-        sampleTextField.frame.origin = CGPoint(x: w/2-75, y: y-10-40-10)
-//        sampleTextField.delegate = self
-        sampleTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+        movieTextField.frame.origin = CGPoint(x: w/2-75, y: y-10-40-10)
+        // movieTextField.delegate = self
+        movieTextField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         
         
         // Swipe
-        
-        
-//        let gesture = UISwipeGestureRecognizer()
-//        gesture.addTarget(self, action: #selector(handleSwipe(_:)))
-//        gesture.direction = .up
-//        self.view.addGestureRecognizer(gesture)
-        
-        
         let directions: [UISwipeGestureRecognizerDirection] = [.right, .left, .up, .down]
         for direction in directions {
             let gesture = UISwipeGestureRecognizer()
             gesture.addTarget(self, action: #selector(handleSwipe(_:)))
             
-//            let gesture = UISwipeGestureRecognizer(target: self, action: Selector(("handleSwipe:")))
+            // let gesture = UISwipeGestureRecognizer(target: self, action: Selector(("handleSwipe:")))
             gesture.direction = direction
             self.view.addGestureRecognizer(gesture)
         }
-        
-        //Looks for single or multiple taps.
-        
-        
-        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
-        //tap.cancelsTouchesInView = false
         
         
         
@@ -148,11 +170,18 @@ class ViewController: DatasourceController {
         // https://www.charted.co/c/0d5349f
         
         /* ADD OBJECTS */
-        self.view.addSubview(mySlider)
-        self.view.addSubview(mySlider2)
-        self.view.addSubview(btn)
-        self.view.addSubview(btn2)
-        self.view.addSubview(sampleTextField)
+        // Sliders
+        self.view.addSubview(interest)
+        self.view.addSubview(cliche)
+        self.view.addSubview(funny)
+        self.view.addSubview(dumb)
+        self.view.addSubview(wtf)
+        // Buttons
+        self.view.addSubview(btnStart)
+        self.view.addSubview(btnPlay)
+        // Text Field
+        self.view.addSubview(movieTextField)
+        // Show Hide Buttons
         self.view.addGestureRecognizer(tap)
         
         
@@ -163,11 +192,10 @@ class ViewController: DatasourceController {
         ref = Database.database().reference()
         
         Auth.auth().signIn(withEmail: "riodweber@gmail.com", password: "asdfasdf") { (user, error) in
-            self.ref.child("users").setValue(["username": "Weber"])
-            self.ref.child("last_sign_in").setValue(["time": NSDate().timeIntervalSince1970])
-            
             let userID = Auth.auth().currentUser?.uid
             print("USERID: " + userID!)
+            
+            self.ref.child("users").child(userID!).setValue(["last_sign_in_time": NSDate().timeIntervalSince1970])
         }
         
         /* LOGIC */
@@ -175,6 +203,7 @@ class ViewController: DatasourceController {
         
     }
     
+    // Text Field (Movie Name)
     @objc func textFieldDidChange(_ textField: UITextField) {
         if ((textField.text) != nil && (textField.text) != "") {
             if (textField.text?.last! == " ") {
@@ -189,23 +218,59 @@ class ViewController: DatasourceController {
         
     }
     
+    // Swipe Up/Down
     @objc func handleSwipe(_ sender: UISwipeGestureRecognizer!) {
-//        print(sender.direction)
-        
+        // SHOW
         if (sender.direction == UISwipeGestureRecognizerDirection.up) {
-            self.btn.frame.origin = CGPoint(x: 0, y: y-50)
-            self.btn2.frame.origin = CGPoint(x: w-100, y: y-50)
+            self.btnStart.frame.origin = CGPoint(x: 0, y: y-50)
+            self.btnPlay.frame.origin = CGPoint(x: w-100, y: y-50)
+            movieTextField.frame.origin = CGPoint(x: w/2-75, y: y-10-40-10)
+            
+            // SLIDERS
+            // interest
+            self.interest.bounds.size.width = y-150
+            interest.center = CGPoint(x: w/2-((20*5)-(0*50)), y: y/2+10)
+            // cliche
+            self.cliche.bounds.size.width = y-150
+            cliche.center = CGPoint(x: w/2-((20*5)-(1*50)), y: y/2+10)
+            // funny
+            self.funny.bounds.size.width = y-150
+            funny.center = CGPoint(x: w/2-((20*5)-(2*50)), y: y/2+10)
+            // dumb
+            self.dumb.bounds.size.width = y-150
+            dumb.center = CGPoint(x: w/2-((20*5)-(3*50)), y: y/2+10)
+            // wtf
+            self.wtf.bounds.size.width = y-150
+            wtf.center = CGPoint(x: w/2-((20*5)-(4*50)), y: y/2+10)
         }
+        // HIDE
         if (sender.direction == UISwipeGestureRecognizerDirection.down) {
-            self.btn.frame.origin = CGPoint(x: 0, y: y-10)
-            self.btn2.frame.origin = CGPoint(x: w-100, y: y-10)
+            self.btnStart.frame.origin = CGPoint(x: 0, y: y-10)
+            self.btnPlay.frame.origin = CGPoint(x: w-100, y: y-10)
+            movieTextField.frame.origin = CGPoint(x: w/2-75, y: y-10)
+            
+            // SLIDERS
+            // interest
+            self.interest.bounds.size.width = y-100
+            interest.center = CGPoint(x: w/2-((20*5)-(0*50)), y: y/2+30)
+            // cliche
+            self.cliche.bounds.size.width = y-100
+            cliche.center = CGPoint(x: w/2-((20*5)-(1*50)), y: y/2+30)
+            // funny
+            self.funny.bounds.size.width = y-100
+            funny.center = CGPoint(x: w/2-((20*5)-(2*50)), y: y/2+30)
+            // dumb
+            self.dumb.bounds.size.width = y-100
+            dumb.center = CGPoint(x: w/2-((20*5)-(3*50)), y: y/2+30)
+            // wtf
+            self.wtf.bounds.size.width = y-100
+            wtf.center = CGPoint(x: w/2-((20*5)-(4*50)), y: y/2+30)
         }
-        
     }
     
     
     
-   
+   // SLIDER CHANGED
     @objc func sliderValueDidChange(_ sender2:UISlider!) {
 //        print("Slider value changed")
         
@@ -228,23 +293,30 @@ class ViewController: DatasourceController {
 //        NSLog("counting...")
         
         if (self.start && self.pause) {
-            let roundedStepValue: Float = round(self.mySlider.value / step) * step
-            let roundedStepValue2: Float = round(self.mySlider2.value / step) * step
+            let interestValue: Float = round(self.interest.value / step) * step
+            let clicheValue: Float = round(self.cliche.value / step) * step
+            let funnyValue: Float = round(self.funny.value / step) * step
+            let dumbValue: Float = round(self.dumb.value / step) * step
+            let wtfValue: Float = round(self.wtf.value / step) * step
             
-//            self.mySlider.value = roundedStepValue
-            print("Slider 1 value \(Int(roundedStepValue))")
-            print("Slider 2 value \(Int(roundedStepValue2))")
+            // self.mySlider.value = roundedStepValue
+            // print("Slider 1 value \(Int(interestValue))")
+            // print("Slider 2 value \(Int(clicheValue))")
             
             // push change
-            self.ref.child(self.movie_name).childByAutoId().setValue(roundedStepValue)
-            self.ref.child(self.movie_name + "2").childByAutoId().setValue(roundedStepValue2)
+            self.ref.child("movies").child(self.movie_name).child("interest").childByAutoId().setValue(interestValue)
+            self.ref.child("movies").child(self.movie_name).child("cliche").childByAutoId().setValue(clicheValue)
+            self.ref.child("movies").child(self.movie_name).child("funny").childByAutoId().setValue(funnyValue)
+            self.ref.child("movies").child(self.movie_name).child("dumb").childByAutoId().setValue(dumbValue)
+            self.ref.child("movies").child(self.movie_name).child("wtf").childByAutoId().setValue(wtfValue)
         } else {
+            // Not On
             print("OFF")
         }
         
     }
     
-    /* BUTTON (Start, Stop) */
+    /* BUTTON (START, STOP) */
     @objc func buttonAction(sender: UIButton!) {
 //        let btnsendtag: UIButton = sender
         
@@ -255,12 +327,19 @@ class ViewController: DatasourceController {
 //        }
         if (self.start == false) {
             self.start = true
-            self.btn.backgroundColor = UIColor.red
-            self.btn.setTitle("STOP", for: .normal)
+            self.btnStart.backgroundColor = UIColor.red
+            self.btnStart.setTitle("STOP", for: .normal)
         } else {
             self.start = false
-            self.btn.backgroundColor = UIColor.green
-            self.btn.setTitle("START", for: .normal)
+            self.btnStart.backgroundColor = UIColor.green
+            self.btnStart.setTitle("START", for: .normal)
+            
+            // Reset Sliders
+            self.interest.value = 0
+            self.cliche.value = 0
+            self.funny.value = 0
+            self.dumb.value = 0
+            self.wtf.value = 0
         }
     }
     
@@ -275,12 +354,12 @@ class ViewController: DatasourceController {
         //        }
         if (self.pause == false) {
             self.pause = true
-            self.btn2.backgroundColor = UIColor.blue
-            self.btn2.setTitle("Pause", for: .normal)
+            self.btnPlay.backgroundColor = UIColor.blue
+            self.btnPlay.setTitle("Pause", for: .normal)
         } else {
             self.pause = false
-            self.btn2.backgroundColor = UIColor.green
-            self.btn2.setTitle("Play", for: .normal)
+            self.btnPlay.backgroundColor = UIColor.green
+            self.btnPlay.setTitle("Play", for: .normal)
         }
     }
     
@@ -290,7 +369,6 @@ class ViewController: DatasourceController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     
     
 }
